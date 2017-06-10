6a0f67cbd66dc4194d624f416e0c3301884caadc
ali <ali@ali-PC>
2017-06-10 08:43:08 -0600

    New project files to use with Hello World project

---
+++ src/edu/cnm/deepdive/helloworld/Helloworld.java
@@ -0,0 +1,23 @@
+//TODO Auto-generated method stub
+"Hello World"/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * @author ali
+ *
+ */
+public class Helloworld {
+
+  /**
+   * Prints "Hello World1" to standard output.
+   * 
+   * @param args  Command-line parameters (ignored).
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
