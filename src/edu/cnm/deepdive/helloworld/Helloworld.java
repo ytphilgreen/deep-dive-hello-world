8bcc114e14033e0010664ddf82c70fc2156a788a
ali <ali@ali-PC>
2017-06-12 09:44:17 -0600

    unnecessary decompostion because Nick said so...!

--- src/edu/cnm/deepdive/helloworld/Helloworld.java
+++ src/edu/cnm/deepdive/helloworld/Helloworld.java
@@ -1,23 +1,26 @@
 
- /**
- * 
- */
+/**
+* 
+*/
 package edu.cnm.deepdive.helloworld;
 
 /**
- * @author ali
- * Simple class that displays "Hello World!" message.
+ * @author ali Simple class that displays "Hello World!" message.
  */
 public class Helloworld {
 
   /**
    * Prints "Hello World!" to standard output.
    * 
-   * @param args  Command-line parameters (ignored).
+   * @param args Command-line parameters (ignored).
    */
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    emitMessage("Hello World!");
+
 
   }
 
+  private static void emitMessage(String message) {
+    System.out.println(message);
+  }
 }
