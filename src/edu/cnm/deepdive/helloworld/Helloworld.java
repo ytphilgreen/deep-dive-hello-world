0681443a7072359049783965bd1ce3c59e2759cc
ali <ali@ali-PC.admin.ad.cnm.edu>
2017-06-12 15:00:27 -0600

    Added message arguments

--- src/edu/cnm/deepdive/helloworld/Helloworld.java
+++ src/edu/cnm/deepdive/helloworld/Helloworld.java
@@ -17,15 +17,17 @@ public class Helloworld {
   public static void main(String[] args) {
     String target;
     String message;
-    if (args.length >0){
-      target = args [0];
-    } else{
+    if (args.length > 0) {
+      target = args[0];
+    } else {
       target = "World";
     }
-    message = String.format ("Hello %s!", target);
+    message = String.format("Hello %s!", target);
     emitMessage(message);
+    for (int i = 0; i < args.length; i++) {
 
-
+      System.out.printf("Arg # %d is %s%n", i, args[i]);
+    }
   }
 
   private static void emitMessage(String message) {
